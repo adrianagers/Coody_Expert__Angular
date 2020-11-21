c5deb348f968dfd94c5a5d83d3b7d3ba7e070608
Daniela Lombana (CO) <daniela.lombana@bcdtravel.com.co>
2020-11-21 18:56:35 -0500

    Subo cambios

---
+++ src/app/Models/User.ts
@@ -0,0 +1,10 @@
+export interface User{
+    id: String;
+    nombre: String;
+    apellido: String; 
+    correo: String; 
+    telefono: Number; 
+    password: String;
+    ciudad: String; 
+    aceptarPoliticas: Boolean; 
+}
