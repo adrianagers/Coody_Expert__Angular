35102106beaeffe0eaba50d9623507a6c00e18e7
dcalderonalegre@gmail.com <dcalderonalegre@gmail.com>
2020-11-24 08:34:08 -0500

    Fron de Angular-Empresarial y Programador con sus respectivos Estilos

---
+++ src/app/Components/inicio-empresa/inicio-empresa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-empresa',
+  templateUrl: './inicio-empresa.component.html',
+  styleUrls: ['./inicio-empresa.component.css']
+})
+export class InicioEmpresaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
